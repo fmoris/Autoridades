91134ffde07d8dfe1a75e90bf6d1e2a2de271bd9
Francisco Moris <fmoris@gmail.com>
2021-08-16 15:12:13 -0400

    responvise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,35 @@ export class AppComponent implements OnInit {
     this.auths = this.autoridadesService.getAutoridades();
   }
   ngOnInit(): void {
-
-    this.breakpoint = (window.innerWidth <= 400) ? 1 : 4;
+    this.breakpoint = this.calulateBreakPoint();
     this.rowHeight = this.breakpoint === 1 ? '1: 1.3' : '1:1.4'
   }
 
   onResize(event: any) {
-    console.log(event, this.breakpoint);
-    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 4;
+    console.log(event.target.innerWidth);
+    console.log(this.calulateBreakPoint());
+    this.breakpoint = this.calulateBreakPoint();
     this.rowHeight = this.breakpoint === 1 ? '1: 1.3' : '1:1.4'
+  }
+
+  calulateBreakPoint(): number {
+    let bp: number = 1;
+    if (window.innerWidth <= 480) {
+      //Mobile devices
+      bp = 1
+    } else if (window.innerWidth >= 481 && window.innerWidth <= 768) {
+      //iPads, Tablets
+      bp = 2
+    } else if (window.innerWidth >= 769 && window.innerWidth <= 1024) {
+      // Small screens, laptops
+      bp = 3
+    } else if (window.innerWidth >= 1025 && window.innerWidth <= 1200) {
+      // Desktops, large screens
+      bp = 4
+    } else if (window.innerWidth > 1200) {
+      // Extra large screens, TV
+      bp = 4
+    }
+    return bp;
   }
 }
